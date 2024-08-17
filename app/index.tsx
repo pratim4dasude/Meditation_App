b3c9ebcf14982577150baf16f13630a2ba46c798
pratim dasude <pratim4dasude@gmail.com>
2024-08-18 00:34:01 +0530

    Added CustomButton

--- app/index.tsx
+++ app/index.tsx
@@ -8,16 +8,44 @@ import beachImage from "@/assets/meditation-images/beach.webp"
 import { View, Text, ImageBackground } from 'react-native'
 import React from 'react'
 
+import { LinearGradient } from "expo-linear-gradient";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { StatusBar } from "expo-status-bar";
+
+import  CustomButton  from "@/components/CustomButton"
+
 const App = () => {
   return (
     <View className="flex-1">
 
       <ImageBackground source={beachImage} resizeMode="cover" className="flex-1">
-        
-        <Text>App</Text>
+        <LinearGradient className="flex-1" colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+          <SafeAreaView className="flex-1 mx=5 my-12 justify-between">
+            <View>
+              <Text className="text-center text-white font-bold text-4xl">Meditation</Text>
+              <Text className="text-center text-white text-regular text-2xl mt-3">
+                Simplyfing meditation for Everyone
+              </Text>
+            </View>
+
+            <View>
+              <CustomButton onPress={() => console.log("tap")} title="get started" />
+
+
+            </View>
+
+
+            <StatusBar style="light" />
+
+          </SafeAreaView>
+
+        </LinearGradient>
+
       </ImageBackground>
 
-      
+
     </View>
   )
 }
